eeb0b5925ecbf04661358d0dc99a11e204ac5f43
Blumel Token <blumeltoken@example.com>
2026-02-01 16:26:51 +0100

    webarchive version in archive

---
+++ archive/wp-content/themes/cryptoking/assets/js/custom/cryptoking_roadmap_ver-1.0.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+jQuery(document).ready(function( $ ) {
+ "use strict";
+ 
+	 $('.roadmap').owlCarousel({
+	     loop: false,
+	     margin: 30,
+	     nav: true,
+	     navText: ['<i class="ion-ios-arrow-back"></i>', '<i class="ion-ios-arrow-forward"></i>'],
+	     responsive: {
+	         0: {
+	             items: 1,
+
+	         },
+	         380: {
+	             items: 2,
+	             margin: 15,
+	         },
+	         600: {
+	             items: 3
+	         },
+	         1000: {
+	             items: 5
+	         },
+	         1199: {
+	             items: 5
+	         }
+	     }
+	 });
+		
+}); 
+}
